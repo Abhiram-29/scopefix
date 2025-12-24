6dd8dd9958da2e3250f99b4f2962423650732645
Abhiram-29 <abhirampabc@gmail.com>
2025-12-24 16:45:01 +0000

    L2 implemented

--- fix_loop.py
+++ fix_loop.py
@@ -1,14 +1,15 @@
-from bandit_analysis import analyze_code,verify_patch
+from bandit_analysis import analyze_code
 from bandit_doc_scrape import scrape_bandit_docs
 from strategist import vuln_strategist
 from schema import GraphState
 from langgraph.graph import StateGraph, START, END
-from patchers import small_patcher
+from patchers import junior_patcher, senior_patcher
 from dotenv import load_dotenv
 import re
 from pathlib import Path
+from typing import Literal
 
-curr_file = "py_dst/sample_0.py"
+curr_file = "py_dst/sample_10.py"
 
 load_dotenv()
 bandit_analysis = analyze_code(curr_file)
@@ -16,6 +17,13 @@ vuln_notes = []
 
 def load_code(file_path: str) -> str:
     return Path(file_path).read_text(encoding="utf-8")
+
+def leftover_checker(state: GraphState) -> Literal["senior_patcher","end_workflow"]:
+    leftovers = state.get("processed_vulnerabilities",[])
+    if leftovers:
+        return "senior_patcher"
+    else:
+        return "end_workflow"
 
 for vuln in bandit_analysis:
     url = vuln['more_info_url']
@@ -29,10 +37,16 @@ for vuln in bandit_analysis:
 
 workflow = StateGraph(GraphState)
 workflow.add_node("vuln_strategist",vuln_strategist)
-workflow.add_node("small_patcher",small_patcher)
+workflow.add_node("junior_patcher",junior_patcher)
+workflow.add_node("senior_patcher",senior_patcher)
 workflow.add_edge(START,"vuln_strategist")
-workflow.add_edge("vuln_strategist","small_patcher")
-workflow.add_edge("small_patcher",END)
+workflow.add_edge("vuln_strategist","junior_patcher")
+workflow.add_conditional_edges(
+    "junior_patcher",
+    leftover_checker,
+    {"senior_patcher":"senior_patcher", "end_workflow":END}
+    )
+workflow.add_edge("senior_patcher",END)
 app = workflow.compile()
 
 py_code = load_code(curr_file)
@@ -41,4 +55,4 @@ res=app.invoke({"raw_vulnerabilities":vuln_notes, "code":py_code})
 
 for strat in res["processed_vulnerabilities"]:
     print(strat)
-print(res["patch_results"])
+print(res["code"])
--- patchers.py
+++ patchers.py
@@ -2,14 +2,57 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from bandit_analysis import verify_patch
+from functools import partial
 from schema import GraphState
 
-def small_patcher(state: GraphState):
+def patching_logic(state: GraphState, model: str, level: int, patch_prompt: ChatPromptTemplate):
     vulnerabilities = state['processed_vulnerabilities']
-    patch_results = []    
-    llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
+    patch_results = state.get("patch_results",[])
+    curr_code = state["code"]
+    leftover_vulnerabilities = []    
+    llm = ChatGoogleGenerativeAI(model=model)
+   
+    chain = patch_prompt | llm | StrOutputParser()
+    
+    print(f"Attempting Level {level} Patch (Small LLM) for {len(vulnerabilities)} issues")
 
-    patch_prompt = ChatPromptTemplate.from_template(
+    for vuln in vulnerabilities:
+        strategy,test_id = vuln["strategy"],vuln["test_id"]
+        print(test_id)
+        try:
+            fixed_code = chain.invoke({
+                "code_snippet": curr_code,
+                "fix_strategy": strategy
+            })
+            fixed_code = fixed_code.replace("```python", "").replace("```", "").strip()
+            is_fixed = verify_patch(fixed_code, test_id)         
+            if is_fixed:
+                curr_code = fixed_code
+            else:
+                leftover_vulnerabilities.append(vuln)
+            
+            status = "success" if is_fixed else f"failed_level_{level}"
+            print(f"Issue {test_id}: {status}")
+
+            patch_results.append({
+                "patched_code": fixed_code,
+                "status": status,
+                "level_attempted": level
+            })
+            
+        except Exception as e:
+            print(f"Error patching {test_id}: {e}")
+            patch_results.append({
+                "status": "error",
+                "error_msg": str(e),
+                "level": level
+            })
+
+    return {"patch_results": patch_results, "processed_vulnerabilities": leftover_vulnerabilities, "code": curr_code}
+
+
+
+l1_patch_prompt = ChatPromptTemplate.from_template(
         """
         You are a cybersecurity specialist. Given a vulnerable code your job is to fix it without changing its functionality.
         You will be given an explaination of the vulnerability and a strategy to fix it, use the strategy as a guideline to fix the code.
@@ -25,35 +68,25 @@ def small_patcher(state: GraphState):
         Output ONLY the valid Python code. No markdown, no explanations, No code comments.
         """
     )
-    
-    chain = patch_prompt | llm | StrOutputParser()
-    
-    print(f"Attempting Level 1 Patch (Small LLM) for {len(vulnerabilities)} issues")
 
-    for vuln in vulnerabilities:
-        strategy,test_id = vuln["strategy"],vuln["test_id"]
-        print(test_id)
-        try:
-            fixed_code = chain.invoke({
-                "code_snippet": state['code'],
-                "fix_strategy": strategy
-            })
-            fixed_code = fixed_code.replace("```python", "").replace("```", "").strip()
-            is_fixed = verify_patch(fixed_code, test_id)         
-            status = "success" if is_fixed else "failed_level_1"
-            print(f"Issue {test_id}: {status}")
+junior_patcher = partial(patching_logic, model="gemini-3-flash-preview", level=1, patch_prompt=l1_patch_prompt)
 
-            patch_results.append({
-                "patched_code": fixed_code,
-                "status": status, # 'success' or 'failed_level_1'
-                "level_attempted": 1
-            })
-            
-        except Exception as e:
-            print(f"Error patching {test_id}: {e}")
-            patch_results.append({
-                "status": "error",
-                "error_msg": str(e)
-            })
+l2_patch_prompt = ChatPromptTemplate.from_template(
+    """
+    You are a senior cybersecurity specialist. Your job is to patch vulnerable codes that your juniors failed to patch.
+    You will be given a code, an explanation of the vulnerability and a strategy to fix it, patch the code without changing its functionality.
+    Your juniors tried to patch the code using the same strategy but they failed, so think critically and use your own creativity, you can deviate from the strategy
 
-    return {"patch_results": patch_results}
+    VULNERABLE CODE:
+    {code_snippet}
+
+    VULNERABILITY DETAILS AND FIXING STRATEGY:
+    {fix_strategy}
+
+    TASK:
+    Rewrite the code snippet to be secure. 
+    Output ONLY the valid Python code. No markdown, no explanations, No code comments.
+    """
+)
+
+senior_patcher = partial(patching_logic, model="gemini-3-pro-preview", level=2, patch_prompt=l2_patch_prompt)
